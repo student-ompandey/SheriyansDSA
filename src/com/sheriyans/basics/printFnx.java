429a1d68123b7252affb72fa0ebae761afe8562d
Om Pandey <ompandey628782@gmail.com>
2026-05-09 23:15:50 +0530

    if-else

--- src/com/sheriyans/basics/printFnx.java
+++ src/com/sheriyans/basics/printFnx.java
@@ -3,5 +3,9 @@ package com.sheriyans.basics;
 public class printFnx {
     public static void main(String[] args) {
         System.out.println("om pandey");
+
+        int age = 10;
+        if(age > 18) System.out.println("You can vote");
+        else System.out.println("You cannot vote");
     }
 }
