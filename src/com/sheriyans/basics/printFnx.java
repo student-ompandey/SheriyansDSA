4b40bfeb342212e1099f7f7ede9b7b3c39c9ecd3
Om Pandey <ompandey628782@gmail.com>
2026-05-12 08:16:38 +0530

    user-input

--- src/com/sheriyans/basics/printFnx.java
+++ src/com/sheriyans/basics/printFnx.java
@@ -1,11 +1,22 @@
 package com.sheriyans.basics;
 
+import java.util.Scanner;
+
 public class printFnx {
     public static void main(String[] args) {
-        System.out.println("om pandey");
+//        System.out.println("om pandey");
 
-        int age = 10;
-        if(age > 18) System.out.println("You can vote");
-        else System.out.println("You cannot vote");
+//        int age = 10;
+//        if(age > 18) System.out.println("You can vote");
+//        else System.out.println("You cannot vote");
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the first number : ");
+        int a = sc.nextInt();
+        System.out.println("Enter the secound number : ");
+        int b = sc.nextInt();
+        int sum = a + b ;
+        System.out.println("Sum of a and b = " + sum);
+
     }
 }
