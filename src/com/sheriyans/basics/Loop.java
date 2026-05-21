2eb8f11cf2951537a9082fdffc9db5ecb5a7c2f6
Om Pandey <ompandey628782@gmail.com>
2026-05-21 19:38:32 +0530

    for loop

---
+++ src/com/sheriyans/basics/Loop.java
@@ -0,0 +1,15 @@
+package com.sheriyans.basics;
+
+import java.util.Scanner;
+
+public class Loop {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        for(int i=0;i<n;i++){
+            System.out.println("Om Pandey");
+        }
+
+    }
+}
