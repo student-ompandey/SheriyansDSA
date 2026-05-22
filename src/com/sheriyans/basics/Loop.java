0c02572fe341e52e4797135eb1f259373cb9b77a
Om Pandey <ompandey628782@gmail.com>
2026-05-22 19:40:27 +0530

    prime

--- src/com/sheriyans/basics/Loop.java
+++ src/com/sheriyans/basics/Loop.java
@@ -10,11 +10,37 @@ public class Loop {
 //        for(int i=0;i<n;i++){
 //            System.out.println("Om Pandey");
 //        }
-        int sum = 0;
-        for (int i=1;i<=n;i++){
-            sum += i;
+//        int sum = 0;
+//        for (int i=1;i<=n;i++){
+//            sum += i;
+//        }
+//        System.out.println(sum);
+
+//        for(int i=1;i<=Math.sqrt(n);i++){
+//            if(n%i==0){
+//                System.out.print(i +" ");
+//
+//            }
+//            if(i != n / i) {
+//                System.out.print(i);
+//            }
+//            }
+//        System.out.print(" "+n);
+
+        int count = 0;
+        for(int i=2;i<=n/2;i++){
+            if(n%i==0){
+                count = 1;
+            }
+
         }
-        System.out.println(sum);
+        if(count==0) System.out.println("prime");
+        else System.out.println("not prime");
+
+
+
 
     }
-}
+
+    }
+
