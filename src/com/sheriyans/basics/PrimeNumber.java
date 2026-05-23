86b44ad94818bfd3928f29ad5f3ef619839edadb
Om Pandey <ompandey628782@gmail.com>
2026-05-23 19:15:26 +0530

    prime optimized

---
+++ src/com/sheriyans/basics/PrimeNumber.java
@@ -0,0 +1,26 @@
+package com.sheriyans.basics;
+
+import java.util.Scanner;
+
+public class PrimeNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        if(n<=1) System.out.println("not prime");
+        else if(n==2) System.out.println("prime");
+        else if(n%2==0) System.out.println("not prime");
+        else {
+            boolean isPrime = true;
+            for(int i=3;i<Math.sqrt(n);i+=2){
+                if(n%i==0){
+                    isPrime = false;
+                    break;
+                }
+            }
+            if(isPrime) System.out.println("prime");
+            else System.out.println("not prime");
+        }
+
+
+    }
+}
