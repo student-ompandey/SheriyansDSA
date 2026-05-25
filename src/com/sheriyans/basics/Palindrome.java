f95ff968a9adc6958db7df5be04d4d4bfaa52e8e
Om Pandey <ompandey628782@gmail.com>
2026-05-25 19:43:02 +0530

    palindrome

---
+++ src/com/sheriyans/basics/Palindrome.java
@@ -0,0 +1,22 @@
+package com.sheriyans.basics;
+
+import java.util.Scanner;
+
+public class Palindrome {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        int o = n;
+        int rev = 0;
+
+        while(n>0){
+            int ld = n%10;
+             rev  = rev * 10 + ld;
+            n = n/10;
+        }
+
+        if(o==rev) System.out.println("palindrome");
+        else System.out.println("not palindrome");
+    }
+}
