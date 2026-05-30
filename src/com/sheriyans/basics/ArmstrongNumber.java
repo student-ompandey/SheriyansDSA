b0efb46a9679d65f7d0c90f5c1caf716740b601d
Om Pandey <ompandey628782@gmail.com>
2026-05-30 19:34:40 +0530

    Armstrong Number

---
+++ src/com/sheriyans/basics/ArmstrongNumber.java
@@ -0,0 +1,29 @@
+package com.sheriyans.basics;
+
+import java.util.Scanner;
+
+public class ArmstrongNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int count = 0;
+        int num = n;
+        int f = n;
+        while(n!=0){
+            int ld = n % 10;
+            count++;
+            n = n / 10;
+        }
+        int ans = 0;
+
+        while(num!=0){
+            int ld = num %10;
+            int q = (int) Math.pow(ld, count);
+            ans += q;
+            num = num / 10;
+        }
+
+        if(ans == f) System.out.println("armstrong");
+        else System.out.println("not");
+    }
+}
