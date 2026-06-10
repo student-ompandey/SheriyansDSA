139b23215c1521650e5ae7f108c288968bac82fd
Om Pandey <ompandey628782@gmail.com>
2026-06-10 19:13:39 +0530

    Right rotate

---
+++ src/Arrays/RightRotate.java
@@ -0,0 +1,21 @@
+package Arrays;
+
+public class RightRotate {
+    public static void main(String[] args) {
+        int arr[] = {1, 2, 3, 4};
+        int n = arr.length;
+        // right rotation by 1
+//        int temp = arr[n-1];
+//        for(int i=n-1;i>=1;i--){
+//            arr[i] = arr[i-1];
+//
+//        }
+//        arr[0] = temp;
+//
+//        for (int i = 0; i < n; i++) {
+//            System.out.print(arr[i]+" ");
+//        }
+
+
+    }
+}
